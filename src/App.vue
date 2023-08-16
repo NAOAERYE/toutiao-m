e5ef32cc03a8a571f7bf3f558181b07f20b044c2
dw729271 <dw729271@163.com>
2023-08-16 17:14:04 +0800

    引入字体图标和vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,19 @@
   <div id="app">
     <router-view/>
     <h1 style="margin: 0 auto;">黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-yuedu"></i>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="success">成功按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="嗨嗨嗨" />
+    </van-cell-group>
+
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入vant组件库
+import Vant from 'vant'
+// 引入组件库样式
+import 'vant/lib/index.css'
 // 引入全局样式文件
-import '@/styles/index.css'
+import '@/styles/index.less'
+// 注册组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
